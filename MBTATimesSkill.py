eaeadff6b2699277ce412577732bd0dea2325f4a
Karanraj Chauhan <chauhank@bu.edu>
2017-07-02 18:11:04 -0400

    added playlist functionality

--- MBTATimesSkill.py
+++ MBTATimesSkill.py
@@ -39,31 +39,6 @@ def getBusTimes(busDestination):
     
     return statement(reply)
 
-'''
-@ask.intent('KenmoreBusIntent')
-def getKenmoreBusTimes():
-    busTimes = getNextBusTimesApi(stop='929')    # 929 to kenmore, 962 to watertown
-    if len(busTimes)>=2:
-        reply = 'The next buses to Kenmore are ' + busTimes[0] + ' and ' + busTimes[1]
-    elif len(busTimes)==1:
-        reply = 'The next bus to Kenmore is ' + busTimes[0]
-    else:
-        reply = 'Sorry, I could not find any buses to Kenmore at this time'
-    return statement(reply)
-
-
-@ask.intent('WatertownBusIntent')
-def getWatertownBusTimes():
-    busTimes = getNextBusTimesApi(stop='962')    # 929 to kenmore, 962 to watertown
-    if len(busTimes)>=2:
-        reply = 'The next buses to Watertown are ' + busTimes[0] + ' and ' + busTimes[1]
-    elif len(busTimes)==1:
-        reply = 'The next bus to Watertown is ' + busTimes[0]
-    else:
-        reply = 'Sorry, I could not find any buses to Watertown at this time'
-    return statement(reply)
-'''
-
 #==============================================================================================#
 #======================================== MBTA T Times ========================================#
 #==============================================================================================#
@@ -81,46 +56,50 @@ def getSubwayTimes(subwayDestination):
     
     return statement(reply)
 
-'''
-@ask.intent('ParkStreetSubwayIntent')
-def getParkStreetSubwayTimes():
-    subwayTimes = getNextSubwayTimesScraping('brico', 'Park Street')    # brico is Packards Corner
-    if len(subwayTimes)>=2:
-        reply = 'The next trains to Park Street are ' + subwayTimes[0] + ' and ' + subwayTimes[1]
-    elif len(subwayTimes)==1:
-        reply = 'The next train to Park Street is ' + subwayTimes[0]
-    else:
-        reply = 'Sorry, I could not find any trains to Park Street at this time'
-    return statement(reply)
-
-
-@ask.intent('BostonCollegeSubwayIntent')
-def getBostonCollegeSubwayTimes():
-    subwayTimes = getNextSubwayTimesScraping('brico', 'Boston College')    # brico is Packards Corner
-    if len(subwayTimes)>=2:
-        reply = 'The next trains to Boston College are ' + subwayTimes[0] + ' and ' + subwayTimes[1]
-    elif len(subwayTimes)==1:
-        reply = 'The next train to Boston College is ' + subwayTimes[0]
-    else:
-        reply = 'Sorry, I could not find any trains to Boston College at this time'
-    return statement(reply)
-'''
-
 #==============================================================================================#
 #======================================== Music Player ========================================#
 #==============================================================================================#
 
-songUrls = {
+allSongsUrls = {
 	'despacito': 'https://archive.org/download/LuisFonsi_324/LuisFonsi-Despacitoft.DaddyYankee.ogg',
 	'sugar how you get so fly': 'https://archive.org/download/RobinSchulzSugarfeat.FrancescoYatesOFFICIALMUSICVIDEO_201611/Robin%20Schulz%20-%20Sugar%20(feat.%20Francesco%20Yates)%20(OFFICIAL%20MUSICVIDEO).ogg',
 	'photograph': 'https://archive.org/download/Photograph_201512/Photograph.ogg',
-	'sweet child o mine': 'https://ia801608.us.archive.org/26/items/GunsNRosesSweetChildOMine_201703/Guns%20N%20Roses%20-%20Sweet%20Child%20O%20Mine.ogg'		
+	'sweet child o mine': 'https://ia801608.us.archive.org/26/items/GunsNRosesSweetChildOMine_201703/Guns%20N%20Roses%20-%20Sweet%20Child%20O%20Mine.ogg',	
+    'bailando': 'https://archive.org/details/BailandoSpanishVersion'
 }
+
+playlists = {
+    'chill' = [
+        'https://archive.org/download/LuisFonsi_324/LuisFonsi-Despacitoft.DaddyYankee.ogg',
+        'https://archive.org/download/RobinSchulzSugarfeat.FrancescoYatesOFFICIALMUSICVIDEO_201611/Robin%20Schulz%20-%20Sugar%20(feat.%20Francesco%20Yates)%20(OFFICIAL%20MUSICVIDEO).ogg',
+        'https://archive.org/details/BailandoSpanishVersion'
+    ]
+}
+
+
+currentPlaylistName = None
+currentSongIndex = 0
 
 @ask.intent('MusicIntent')
 def playSong(songName):
-	url = songUrls[str(songName)]
+	url = allSongsUrls[str(songName)]
 	return audio('Playing ' + str(songName)).play(url)
+
+@ask.intent('StartPlaylistIntent')
+def startPlaylist(playlistName):
+    currentPlaylistName = playlistName
+    return audio('Playing ' + str(playlistName) + ' playlist').play(playlists[str(playlistName).lower()][0])
+
+@ask.playback_nearly_finished()
+def queueNextSongInPlaylist():
+    if currentSongIndex < len(playlists[str(currentPlaylistName)])-1:
+        currentSongIndex += 1
+        return audio.enqueue(playlists[currentPlaylistName][currentSongIndex])
+
+@ask.on_playback_finished()
+def notifyIfEndOfPlaylist():
+    if currentSongIndex == len(playlists[currentPlaylistName])-1:
+        return statement('There are no more songs in this playlist!')
 
 @ask.intent('AMAZON.PauseIntent')
 def pausedemo():
